f73edf14ac232e4a846d2deff956cd6a8b3e1027
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2022-05-10 17:56:07 +1000

    Add tilemap and collisions

--- src/screens/Game.lua
+++ src/screens/Game.lua
@@ -7,11 +7,20 @@ function GameScreen.new()
   local self = Screen.new()
 
   local push = require("lib.push")
+  local Tilemapper = require("lib.tilemapper")
+  local bump = require("lib.bump")
 
+  local world = {}
+  local map = {}
   local game = {}
 
   function self:init()
     game = love.graphics.newImage("assets/game-background.png", nil)
+    map = Tilemapper(
+              "assets/gamebase.ldtk",
+              { aseprite = true, collisions = { [1] = true } })
+    world = bump.newWorld()
+    map:loadLevel("Level_0", world)
   end
 
   function self:update()
@@ -25,6 +34,15 @@ function GameScreen.new()
   function self:draw()
     push:start()
     love.graphics.draw(game)
+    map:draw()
+    -- -- useful to debug collisions
+    -- local items = world:getItems()
+    -- for i = 1, #items do
+    --   local item = items[i]
+    --   if item.x and item.y and item.w and item.h then
+    --     love.graphics.rectangle("line", item.x, item.y, item.w, item.h)
+    --   end
+    -- end
     push:finish()
   end
 
